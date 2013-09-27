d0197994b697242415099ff88ec2ad60d04a70fb
Sam <w1sh888@hotmail.com>
2013-09-27 10:21:31 +0700

    Chapter 4: Rails' Falvoured Ruby

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,26 @@ describe "Static pages" do
 
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 	describe "Home page" do
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title', 
-				:text => "#{base_title} | Home")
-		end
-
 		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
+		end
+
+#		it "should have the title 'Home'" do
+#			visit '/static_pages/home'
+#			page.should have_selector('title', 
+#				:text => "#{base_title} | Home")
+#		end
+
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => '| Home')
 		end
 	end
 
@@ -20,7 +31,7 @@ describe "Static pages" do
 		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
-	    end
+		 end
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
@@ -33,7 +44,7 @@ describe "Static pages" do
 		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
-	    end
+		end
 
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
